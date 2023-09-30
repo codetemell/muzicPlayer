5e6d9a10599fa579d2fcb6173fbbea3214f8afe0
codetemell <temelleldarado@gmail.com>
2023-09-30 07:06:00 +0500

    added

---
+++ main.js
@@ -0,0 +1,51 @@
+let progress=document.getElementById("progress")
+let song=document.getElementById("song")
+let ctrlicon=document.getElementById("ctrlicon")
+let containerr=document.querySelector(".containerr")
+let menu=document.querySelector(".menu")
+song.onloadedmetadata=function(){
+    progress.max=song.duration;
+    progress.value=song.currentTime;
+}
+function  playPause(){
+    if(ctrlicon.classList.contains("fa-pause"))
+{
+    song.pause()
+    ctrlicon.classList.remove("fa-pause")
+    ctrlicon.classList.add("fa-play")
+
+}else{
+    song.play();
+
+
+    ctrlicon.classList.add("fa-pause")
+    ctrlicon.classList.remove("fa-play")
+}
+
+
+}
+
+
+
+if(song.play()){
+    setInterval(()=>{
+progress.value=song.currentTime}, 500);
+}
+
+progress.onchange=function(){
+    song.play();
+    song.currentTime=progress.value;
+    ctrlicon.classList.add("fa-pause")
+    ctrlicon.classList.remove("fa-play")
+}
+
+menu.addEventListener("click",sheik)
+function sheik(){
+    containerr.style.backgroundColor='green'
+    song.play();
+
+
+    ctrlicon.classList.add("fa-pause")
+    ctrlicon.classList.remove("fa-play")
+
+}
